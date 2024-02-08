69aeefa6084472a86819b3f44316c854d4fbb1fc
Hilary Lindstrom <hilindst@gmail.com>
2024-02-08 14:28:36 -0600

    updated folder location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_144938) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_29_182346) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "body"
+    t.boolean "published"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -25,4 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_144938) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
+  add_foreign_key "profiles", "users"
 end
